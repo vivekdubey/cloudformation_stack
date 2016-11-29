6b461d1cc7449f315a008e99beab42ceff758677
Vivek Dubey <dubeyv@tabcorp.com.au>
2016-11-29 16:59:26 +1100

    Fixed load path for libraries

--- lib/cloudformation_stack.rb
+++ lib/cloudformation_stack.rb
@@ -1,5 +1,6 @@
-# base_directory = File.expand_path('.',File.dirname(__FILE__))
-Dir.glob("../helper/*.rb").each { |file| require file }
-Dir.glob("cloudformation_stack/*.rb").each { |file| require file }
-Dir.glob("../services/*.rb").each { |file| require file }
-Dir.glob("../rake/*.rake").each { |file| load file }
+base_directory = File.expand_path('../',File.dirname(__FILE__))
+Dir.glob("#{base_directory}/helper/*.rb").each { |file| require file }
+Dir.glob("#{base_directory}/helper/*.rb").each { |file| puts file }
+Dir.glob("#{base_directory}/cloudformation_stack/*.rb").each { |file| require file }
+Dir.glob("#{base_directory}/services/*.rb").each { |file| require file }
+Dir.glob("#{base_directory}/rake/*.rake").each { |file| load file }
