837a4de6bb47476c3f83339017c15ab9c16b03d0
Vivek Dubey <dubeyv@tabcorp.com.au>
2016-11-30 11:17:54 +1100

    Fixed file loading order

--- lib/cloudformation_stack.rb
+++ lib/cloudformation_stack.rb
@@ -1,6 +1,5 @@
 base_directory = File.expand_path('../',File.dirname(__FILE__))
-Dir.glob("#{base_directory}/helper/*.rb").each { |file| require file }
-Dir.glob("#{base_directory}/helper/*.rb").each { |file| puts file }
-Dir.glob("#{base_directory}/cloudformation_stack/*.rb").each { |file| require file }
+Dir.glob("#{base_directory}/lib/cloudformation_stack/*.rb").each { |file| require file }
 Dir.glob("#{base_directory}/services/*.rb").each { |file| require file }
+Dir.glob("#{base_directory}/helper/*.rb").each { |file| require file }
 Dir.glob("#{base_directory}/rake/*.rake").each { |file| load file }
