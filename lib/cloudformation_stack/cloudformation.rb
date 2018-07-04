07db1b80a6b778001feea57c133421d4a80ca382
Nileshb <bairagin@tabcorp.com.au>
2018-07-04 12:10:35 +1000

    adding named_iam capabilities for role updates

--- lib/cloudformation_stack/cloudformation.rb
+++ lib/cloudformation_stack/cloudformation.rb
@@ -17,7 +17,7 @@ class CloudFormation
       @cloud_formation_client.create_stack({
         stack_name: stack_name,
         template_body: template_body,
-        capabilities: ["CAPABILITY_IAM"],
+        capabilities: [ "CAPABILITY_IAM", "CAPABILITY_NAMED_IAM" ],
         parameters: stack_paramters.map{|key, value| {parameter_key: key.to_s, parameter_value: value.to_s, use_previous_value: false}},
         disable_rollback: disable_rollback,
         timeout_in_minutes: 30
@@ -37,7 +37,7 @@ class CloudFormation
       @cloud_formation_client.update_stack({
         stack_name: stack_name,
         template_body: template_body,
-        capabilities: ["CAPABILITY_IAM"],
+        capabilities: [ "CAPABILITY_IAM", "CAPABILITY_NAMED_IAM" ],
         parameters: stack_paramters.map{|key, value| {parameter_key: key.to_s, parameter_value: value.to_s, use_previous_value: false}},
       })
       result = catch(:success) do
